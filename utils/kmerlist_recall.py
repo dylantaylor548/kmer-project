1e677b3aa39980c214f226bb528e65e6ec10346e
Dylan Taylor <dylantaylor548@gmail.com>
2019-01-18 17:23:41 -0500

    Updated recall document to output .csv files

--- utils/kmerlist_recall.py
+++ utils/kmerlist_recall.py
@@ -38,17 +38,17 @@ def main():
     parser = argparse.ArgumentParser(description="Calculates the recall of a given list of kmers on a set of sequences")
     parser.add_argument("-f","--fasta_file", help="A fasta file of sequences",required=True)
     parser.add_argument("-r","--replist_file", help="A csv file containing a representative list of kmers",required=True)
-    parser.add_argument("-s","--stats_out", help="Output file containing data about the proportion of sequences in the fasta file covered by the kmers in the representative list",required=True)
-    parser.add_argument("-m","--sum_out", help="Output file detailing the coverage of each of the sequences in the fasta file",required=True)
+    parser.add_argument("-s","--stats_out", help="Output file containing data about the proportion of sequences in the fasta file covered by the kmers in the representative list",required=False,default='stats')
+    parser.add_argument("-m","--sum_out", help="Output file detailing the coverage of each of the sequences in the fasta file",required=False,default='summary')
     args = parser.parse_args()
 
     fastadict = fasta2dict(args.fasta_file)
     replist = replist2list(args.replist_file)
 
-    stats = open(args.stats_out,'w')
+    stats = open(args.stats_out + '.csv','w')
     stats.write("Total Sequences" + "," + "Covered Sequences" + "," + "Recall" + "\n")
 
-    summary = open(args.sum_out,'w')
+    summary = open(args.sum_out + '.csv','w')
     summary.write("Identifier" + "," + "Covered?" + "," + "Coverage" + "\n")
 
     numseq = len(fastadict)
