0a04b96bba74ded4ead870a68b0f1260100cff19
Dylan Taylor <dtaylo12@cbcbsub00.umiacs.umd.edu>
2019-03-06 15:05:22 -0500

    Removed redundant testing files from the src directory

--- src/forwardjob.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-python kmer_project_master.py -f ../data/TIGR02012/TIGR02012_100.fasta -o f_output.csv
--- src/reversejob.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-python reverse_test.py -f ../data/TIGR02012/TIGR02012_100.fasta -o output.csv -k 15
