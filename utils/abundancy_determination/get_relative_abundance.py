289e1d98d192dd129ad2656b97d8631aa87bf830
Dylan Taylor <dtaylo12@cbcbsub00.umiacs.umd.edu>
2019-04-11 10:56:23 -0400

    Added intermediate spike generation programs to the appropriate folder in utils

--- utils/abundancy_determination/get_relative_abundance.py
+++ utils/abundancy_determination/get_relative_abundance.py
@@ -1,5 +1,6 @@
 import argparse
 from itertools import groupby
+from math import floor
 
 
 def FASTQ_iter(fastq_name):
@@ -68,6 +69,17 @@ def gen_kmerdict(seqdict,oligos):
 	return kmerdict
 
 
+def condense_read_info(reads_in_seqs_dict):
+	seq_presence = {}
+	for oligo in reads_in_seqs_dict:
+		seq_presence[oligo] = {}
+		for seq in reads_in_seqs_dict[oligo]:
+			total_pres = 0
+			for read in reads_in_seqs_dict[oligo][seq]:
+				total_pres += reads_in_seqs_dict[oligo][seq][read]
+			seq_presence[oligo][seq] = total_pres
+	return seq_presence
+
 
 ##############################################################################################################################################################################################
 
@@ -103,6 +115,8 @@ def main():
 		seqs_bound_oligos[oligo] = {}
 
 	overassigned_reads = {}
+	for oligo in chipcovdict:
+		overassigned_reads[oligo] = {}
 
 	for read in readsdict:
 		read_seq = readsdict[read]
@@ -111,37 +125,76 @@ def main():
 			if oligo in read_seq:
 				oligo_matches.append(oligo)
 		if len(oligo_matches) == 1:
+			oligo_match = oligo_matches[0]
 			seq_matches = []
-			for seq in kmerdict[oligo_matches[0]]:
+			for seq in kmerdict[oligo_match]:
 				if read_seq in known_seqs[seq]:
 					seq_matches.append(seq)
 			if len(seq_matches) == 1:
-				seqs_bound_oligos[oligo_matches[0]].setdefault(seq_matches[0],0)
-				seqs_bound_oligos[oligo_matches[0]][seq_matches[0]] += 1
+				seq_match = seq_matches[0]
+				seqs_bound_oligos[oligo_match].setdefault(seq_match,{})
+				seqs_bound_oligos[oligo_match][seq_match].setdefault(read_seq,0)
+				seqs_bound_oligos[oligo_match][seq_match][read_seq] += 1
 			elif len(seq_matches) > 1:
-				overassigned_reads.setdefault(read_seq,0)
-				overassigned_reads[read_seq] += 1
-				for seq in seq_matches:
-					seqs_bound_oligos[oligo_matches[0]].setdefault(seq,0)
+				overassigned_reads[oligo_match].setdefault(read_seq,0)
+				overassigned_reads[oligo_match][read_seq] += 1
 
-	for read_seq, read_count in overassigned_reads.items():
-		for oligo in chipcovdict:
-			if oligo in read_seq:
-				seq_matches = []
-				for seq_id  in seqs_bound_oligos[oligo]:
-					sequence = known_seqs[seq_id]
-					if read_seq in sequence:
-						seq_matches.append(seq_id)
-				for seq_id in seq_matches:
-					quant = (read_count/len(seq_matches))
-					seqs_bound_oligos[oligo][seq_id] += quant
-				break
+	avg_read_cov = {}
+	for oligo in chipcovdict:
+		avg_read_cov[oligo] = {}
+		for seq in seqs_bound_oligos[oligo]:
+			tot_reads = 0
+			counter = 0
+			for read in seqs_bound_oligos[oligo][seq]:
+				tot_reads += seqs_bound_oligos[oligo][seq][read]
+				counter += 1
+			avg_read_cov[oligo][seq] = float(tot_reads/counter)
+
+	for oligo in overassigned_reads:
+		for read in overassigned_reads[oligo]:
+			avail_reads = overassigned_reads[oligo][read]
+			needed_reads = 0
+			for seq in kmerdict[oligo]:
+				if read in known_seqs[seq]:
+					if seq in avg_read_cov[oligo]:
+						needed_reads += avg_read_cov[oligo][seq]
+			if needed_reads <= avail_reads:
+				for seq in kmerdict[oligo]:
+					if read in known_seqs[seq]:
+						if seq in avg_read_cov[oligo]:
+							seqs_bound_oligos[oligo][seq][read] = floor(avg_read_cov[oligo][seq])
+							overassigned_reads[oligo][read] -= floor(avg_read_cov[oligo][seq])
+			elif needed_reads > avail_reads:
+				for seq in kmerdict[oligo]:
+					if read in known_seqs[seq]:
+						if seq in avg_read_cov[oligo]:
+							seqs_bound_oligos[oligo][seq][read] = floor(avg_read_cov[oligo][seq]*(avail_reads/needed_reads))
+							overassigned_reads[oligo][read] -= floor(avg_read_cov[oligo][seq]*(avail_reads/needed_reads))
+	# Once this step is done, we should be left with a dictionary of oligos and reads containing those oligos that have not been attributed to any sequences.
+	# Now what we want to do is to assign these reads evenly to those sequences that have not had any reads assigned to them so far
+
+	for oligo in overassigned_reads:
+		for read in overassigned_reads[oligo]:
+			avail_reads = overassigned_reads[oligo][read]
+			seqs_left = []
+			for seq in kmerdict[oligo]:
+				if read in known_seqs[seq]:
+					if seq not in avg_read_cov[oligo]:
+						seqs_left.append(seq)
+			if len(seqs_left) != 0:
+				quant = floor(avail_reads/len(seqs_left))
+				for seq in seqs_left:
+					seqs_bound_oligos[oligo].setdefault(seq,{})
+					seqs_bound_oligos[oligo][seq].setdefault(read,0)
+					seqs_bound_oligos[oligo][seq][read] += quant
+					overassigned_reads[oligo][read] -= quant
+
+	seq_presence = condense_read_info(seqs_bound_oligos)
+
+	print(seq_presence)
+						
 
 
-	for oligo in seqs_bound_oligos:
-		print(oligo)
-		print(seqs_bound_oligos[oligo])
-		print('')
 
 ##############################################################################################################################################################################################
 
---
+++ utils/synth_spike_generation/get_spike_seqs.py
@@ -0,0 +1,135 @@
+import argparse
+import random
+
+
+def gen_kmerlist(file_path):
+	oligos = []
+
+	with open(file_path) as opened_file:
+		for line in opened_file:
+			line = line.strip()
+			oligos.append(line)
+
+	return oligos
+
+
+def gen_spike_tag(oligos):
+	tag = ''
+	i = 0
+	while i < len(oligos[0]):
+		base_presence = {'A':0,'C':0,'G':0,'T':0}
+		for oligo in oligos:
+			base = oligo[i]
+			base_presence[base] += 1
+		min_pres = None
+		min_base = None
+		for base in base_presence:
+			if min_pres == None:
+				min_base = base
+				min_pres = base_presence[base]
+			elif base_presence[base] < min_pres:
+				min_base = base
+				min_pres = base_presence[base]
+			elif base_presence[base] == min_pres:
+				if random.choice([True,False]):
+					min_base = base
+					min_pres = base_presence[base]
+		tag = tag + min_base
+		i += 1
+	if tag not in oligos:
+		return tag
+	else:
+		while True:
+			print('tag in oligo list, finding new tag...')
+			alphabet = ['A','C','G','T']
+			new_tag = ''
+			for i in range(0,len(oligos[0])):
+				new_tag = new_tag + random.choice(alphabet)
+			if new_tag not in oligos:
+				return new_tag
+
+
+# Generates a list of spike sequences that do not exceed a given length and that have exactly one oligo in any fragment of a given length.
+# Each spike sequence contains the spike tag oligo (same in each spike sequence but unique to the spike sequences) surrounded on each side
+# by 5 random bases allowing distinction between spike sequences on fragments containing the spike tag oligo.
+def gen_spikes(oligos, frag_len, seq_len, spike_tag):
+	alphabet = ['A','T','C','G']
+	spikes = []
+	ids = []
+	
+	i = 0
+	running_len = frag_len
+	while True:
+		spike_id = ''
+		while True:
+			spike_id = random.choice(alphabet) + random.choice(alphabet) + random.choice(alphabet) + random.choice(alphabet) + random.choice(alphabet) + spike_tag + random.choice(alphabet) + random.choice(alphabet) + random.choice(alphabet) + random.choice(alphabet) + random.choice(alphabet)
+			if spike_id not in ids:
+				ids += spike_id
+				print(spike_id)
+				break
+		first_base = spike_id[0]
+		spike = ''
+		if first_base == 'A' or first_base == 'T':
+			spike = (frag_len-len(oligos[i])-5)*'G'
+		elif first_base == 'G' or first_base == 'C':
+			spike = (frag_len-len(oligos[i])-5)*'A'
+		spike = spike + spike_id
+		current_end = spike[-1:]
+		next_start = oligos[i][0]
+		useable_bases = [x for x in alphabet if x != current_end and x != next_start]
+		spike = spike + (frag_len-len(oligos[i])-5)*random.choice(useable_bases)
+
+		while len(spike) + frag_len <= seq_len:
+			oligo = oligos[i]
+			spike = spike + oligo
+			last_base = oligo[-1:]
+			next_base = ''
+			if i+1 < len(oligos):
+				next_base = oligos[i+1][0]
+			new_useable_bases = [x for x in alphabet if x != last_base and x != next_base]
+			spike = spike + (frag_len-len(oligos[i]))*random.choice(new_useable_bases)
+			i += 1
+			if i == len(oligos):
+				spikes.append(spike)
+				return spikes
+		spikes.append(spike)
+
+
+################################################################################
+
+def main():
+	parser = argparse.ArgumentParser(description='Finds a short set of sequences that collectively comprise all kmers within an input kmer list. These synthetic sequences are used to determine relative abundancies when not all sequences are present in a sample.')
+	parser.add_argument("-kl","--kmer_list", help="A .csv file containing the kmer list you are interested in compressing", required=True)
+	parser.add_argument("-n","--seq_len", help="Desired maximum length of synthetic sequences", required=True)
+	parser.add_argument("-s","--frag_size",help="Estimated size of reads produced by fragmenting DNA sample",required=True)
+	parser.add_argument("-o","--out_file", help="A .fasta file to output the synthetic sequences", required=True)
+	parser.add_argument("-i","--fasta_id",help='An identification marker for your output sequences',required=True)
+	parser.add_argument("-t","--spike_tag",help='A unique oligo sequence added to each spike sequence',required=False,default=None)
+	args = parser.parse_args()
+
+	kmerlist = gen_kmerlist(args.kmer_list)
+	print('\n\n' + str(len(kmerlist)) + " kmers in list")
+	print("\nGenerating spike sequences...")
+
+	spike_tag = None
+	if args.spike_tag == None:
+		spike_tag = gen_spike_tag(kmerlist)
+	else:
+		spike_tag = args.spike_tag
+
+	spikes = gen_spikes(kmerlist, int(args.frag_size), int(args.seq_len), spike_tag)
+	print("Done.\n\n" + str(len(spikes)) + " spike sequences generated. Spike tag is '" + spike_tag + "'")
+
+	fw = open(args.out_file,'w')
+	i = 1
+	for spike in spikes:
+		seq_id = ('0000'+str(i))[-4:] + "_" + args.fasta_id
+		fw.write('>' + seq_id + '\n' + spike + '\n')
+		i += 1
+
+	fw.close()
+
+
+################################################################################
+
+main()
